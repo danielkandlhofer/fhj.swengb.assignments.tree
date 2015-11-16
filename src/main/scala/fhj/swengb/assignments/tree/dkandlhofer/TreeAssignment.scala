c124d9a3c638ae88060ec09b671412a176d2a1ea
danielkandlhofer <daniel.kandlhofer@edu.fh-joanneum.at>
2015-11-16 14:34:34 +0100

    Merge branch 'master' of https://github.com/rladstaetter/fhj.swengb.course
    # Conflicts:
    #	material/src/main/scala/fhj/swengb/labs/lab03/JavaFxHelloworld.scala

--- src/main/scala/fhj/swengb/assignments/tree/dkandlhofer/TreeAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tree/dkandlhofer/TreeAssignment.scala
@@ -104,8 +104,8 @@ object L2D {
     * @return
     */
   def apply(start: Pt2D, angle: AngleInDegrees, length: Double, color: Color): L2D = {
-    val endx=Math.sin(toRadiants(angle))*length + start.x
-    val endy=Math.cos(toRadiants(angle))*length + start.y
+    val endx = Math.sin(toRadiants(angle))*length + start.x
+    val endy = Math.cos(toRadiants(angle))*length + start.y
     val end = Pt2D(endx,endy)
     L2D(start,end,color)
   }
